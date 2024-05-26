adef6ca6dbbb0e2d027e2c7f260bd6cb67ae9262
Jheison Cabal <122849556+00Warlock00@users.noreply.github.com>
2024-05-26 15:52:53 -0500

    update avl

--- AVL.cpp
+++ AVL.cpp
@@ -2,6 +2,7 @@
 #include <malloc.h>
 #include <iomanip>
 #include <sstream>
+#include <cmath>
 using namespace std;
 
 struct pasajero
@@ -15,15 +16,14 @@ struct nodo
 {
 
     char destino[45];
-    char matricula[4];
+    int matricula[4];
     char nombre[45];
-    char identificacion[10];
+    int identificacion[10];
 
     int value;
     int anio, mes, dia;
     int embarcacion;
     int altura;
-    pasajero *nuevo;
     nodo *izq;
     nodo *der;
 };
@@ -31,49 +31,139 @@ struct nodo
 struct nodo *raiz = NULL;
 struct nodo *aux, *aux2;
 
-void identificador(char *matricula, int anio, int mes, int dia, char *identificacion)
+void validarMatricula(int matricula[])
 {
-    sprintf(identificacion, "%c%c%04d%02d%02d", matricula[0], matricula[1], anio, mes, dia);
+
+    cout << "Ingrese la matricula (maximo 4 Digitos): ";
+    cin >> matricula;
+    if (matricula < 1000 || matricula > 9999)
+    {
+        cout << "LA MATRICULA DEBE SER DE 4 DIGITOS";
+    }
+    else
+    {
+        // Verificar si la matrícula ingresada tiene más de 4 dígitos
+        int numDigitos = 0;
+        int temp = matricula[0];
+
+        while (temp != 0)
+        {
+            temp /= 10;
+            numDigitos++;
+        }
+
+        if (numDigitos != 4)
+        {
+            cout << "La matricula debe tener exactamente 4 Digitos." << endl;
+        }
+
+        else
+        {
+            cout << "Entrada inválida. Intente nuevamente." << endl;
+        }
+    }
+
+    void validarFecha(int &anio, int &mes, int &dia)
+    {
+        bool esValida = false;
+        while (!esValida)
+        {
+            cout << " INGRESE EL ANIO\n";
+
+            if (anio != 2024)
+            {
+                cout << " EL AÑO DEBE SER EL ACTUAL\n";
+            }
+            else
+            {
+                esValida = true;
+                cout << " INGRESE EL MES\n";
+
+                if (mes < 1 || mes > 12)
+                {
+                    cout << "EL MES DEBE SER DE 1 A 12\n";
+                    esValida = false;
+                }
+                else
+                {
+                    cout << " INGRESE EL DIA\n";
+
+                    if (dia < 1 || dia > 31)
+                    {
+                        cout << " EL DIA DEBE SER DE 1 A 31\n";
+                        esValida = false;
+                    }
+                }
+                else
+                {
+                    cout << "Entrada inválida. Intente nuevamente." << endl;
+                    esValida = false;
+                }
+            }
+
+            else
+            {
+                cout << "Entrada inválida. Intente nuevamente." << endl;
+                esValida = false;
+            }
+        }
+
+        else
+        {
+            cout << "Entrada inválida. Intente nuevamente." << endl;
+        }
+    }
 }
 
-void crearNodo()
+// Función para validar el identificador
+bool validarIdentificador(int identificador[])
 {
-    aux = (struct nodo *)malloc(sizeof(struct nodo));
-    cout << " INGRESE MATRICULA DE LA EMBARCACIÓN\n ";
-    cin >> aux->matricula;
-    cout << " INGRESE NOMBRE DE SU EMBARCACIÓN\n";
-    cin >> aux->nombre;
-    cout << " AHORA LA FECHA:\n";
-    cout << "\n";
-    cout << " AÑO\n";
-    cin >> aux->anio;
-    cout << " MES\n ";
-    cin >> aux->mes;
-    cout << " DÍA\n";
-    cin >> aux->dia;
-    cout << " COSTO:\n";
-    cin >> aux->value;
-    cout << " DESTINO:\n";
-    cin >> aux->destino;
-    cout << "INGRESE LA CAPACIDAD DE SU EMBARCACION:\n";
-    cin >> aux->embarcacion;
-    cout << "\tSU IDENTIFICADOR UNICO ES: \n";
-    identificador(aux->matricula, aux->anio, aux->mes, aux->dia, aux->identificacion);
-    cout << aux->identificacion << endl;
-    aux->izq = NULL;
-    aux->der = NULL;
-    aux->altura = 1;
+    if (identificador == nullptr)
+    {
+        return false;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (identificador[i] < 0 || identificador[i] > 9)
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
-int registrarPasajero(pasajero* cab)
+// Función para imprimir los detalles de un nodo
+void imprimirNodo(nodo *nodoEncontrado)
 {
-    int subopc=0;
-Inorden(raiz);
-cab=(struct pasajero *)malloc(sizeof(struct pasajero));
-cout<<" A QUE EMBARCACION DESEA REGISTRARSE\n";
-cin>>
+    if (nodoEncontrado == nullptr)
+    {
+        cout << "El nodo es nulo" << endl;
+        return;
+    }
 
+    cout << "Identificación: ";
+    for (int i = 0; i < 10; i++)
+    {
+        cout << nodoEncontrado->identificacion[i];
+    }
+    cout << endl;
+    cout << "Matrícula: ";
+    for (int i = 0; i < 4; i++)
+    {
+        cout << nodoEncontrado->matricula[i];
+    }
+    cout << endl;
+    cout << "Nombre: " << nodoEncontrado->nombre << endl;
+    cout << "Año: " << nodoEncontrado->anio << endl;
+    cout << "Mes: " << nodoEncontrado->mes << endl;
+    cout << "Día: " << nodoEncontrado->dia << endl;
+    cout << "Costo: " << nodoEncontrado->value << endl;
+    cout << "Destino: " << nodoEncontrado->destino << endl;
+    cout << "Capacidad: " << nodoEncontrado->embarcacion << endl;
 }
+
 int obtenerAltura(struct nodo *n)
 {
     if (n == NULL)
@@ -185,35 +275,166 @@ void Inorden(nodo *aux3)
         Inorden(aux3->der);
     }
 }
-bool buscarNodoPorIdentificacion(nodo *r, string identificador)
+nodo *BuscarPorIdentificacion(nodo *r, int identificador[])
 {
     if (r == NULL)
     {
-        return false;
+        return NULL;
     }
-    if (r->identificacion == identificador)
+    else
     {
-        return true;
+        bool sonIguales = true; // Inicializar sonIguales a true
+        for (int i = 0; i < 10; i++)
+        {
+            if (r->identificacion[i] != identificador[i])
+            {
+                sonIguales = false;
+                break;
+            }
+        }
+        if (sonIguales)
+        {
+            return r; // Devuelve el puntero al nodo encontrado
+        }
     }
-    return buscarNodoPorIdentificacion(r->izq, identificador) || buscarNodoPorIdentificacion(r->der, identificador);
+
+    nodo *encontrado = BuscarPorIdentificacion(r->izq, identificador);
+    if (encontrado != NULL)
+    {
+        return encontrado; // Devuelve el puntero al nodo encontrado en el subárbol izquierdo
+    }
+    else
+    {
+        return BuscarPorIdentificacion(r->der, identificador); // Busca en el subárbol derecho
+    }
 }
 
+void EliminarPorIdentificacion(nodo *raiz, int eliminar[])
+{
+    if (raiz == NULL)
+    {
+        return;
+    }
+
+    bool sonIguales = true;
+    for (int i = 0; i < 10; i++)
+    {
+        if (raiz->identificacion[i] != eliminar[i])
+        {
+            sonIguales = false;
+            break;
+        }
+    }
+
+    if (sonIguales)
+    {
+        // Eliminar el nodo
+        if (raiz->izq == NULL && raiz->der == NULL)
+        {
+            delete raiz;
+            raiz = NULL;
+        }
+        else if (raiz->izq == NULL)
+        {
+            nodo *temp = raiz;
+            raiz = raiz->der;
+            delete temp;
+        }
+        else if (raiz->der == NULL)
+        {
+            nodo *temp = raiz;
+            raiz = raiz->izq;
+            delete temp;
+        }
+        else
+        {
+            nodo *temp = raiz->der;
+            while (temp->izq != NULL)
+            {
+                temp = temp->izq;
+            }
+            // Copiar el identificador elemento por elemento
+            for (int i = 0; i < 10; i++)
+            {
+                raiz->identificacion[i] = temp->identificacion[i];
+            }
+            EliminarPorIdentificacion(raiz->der, eliminar);
+        }
+    }
+    else if (eliminar[0] < raiz->identificacion[0])
+    {
+        EliminarPorIdentificacion(raiz->izq, eliminar);
+    }
+    else
+    {
+        EliminarPorIdentificacion(raiz->der, eliminar);
+    }
+}
+void crearNodo()
+{
+    aux = (struct nodo *)malloc(sizeof(struct nodo));
+
+    // Validar matrícula
+    cout << " INGRESE MATRICULA DE LA EMBARCACIÓN (MAXIMO 4 DIGITOS)\n ";
+    validarMatricula(aux->matricula);
+
+    // Ingresar nombre de la embarcación
+    cout << " INGRESE NOMBRE DE SU EMBARCACIÓN\n";
+    cin.ignore();                                  // Agregar esta línea
+    cin.getline(aux->nombre, sizeof(aux->nombre)); // Cambio aquí
+
+    // Validar fecha
+    cout << " AHORA LA FECHA:\n\n";
+    validarFecha(aux->anio, aux->mes, aux->dia);
+
+    cout << " COSTO:\n";
+    cin >> aux->value;
+
+    cout << " DESTINO:\n";
+    cin.ignore();
+    cin.getline(aux->destino, sizeof(aux->destino)); // para permitir ingresar cadenas con espacio
+
+    cout << "INGRESE LA CAPACIDAD DE SU EMBARCACION:\n";
+    cin >> aux->embarcacion;
+    if (aux->embarcacion < 1 || aux->embarcacion > 1000)
+    {
+        cout << "Entrada inválida para la capacidad de la embarcación. Operación cancelada." << endl;
+        free(aux);
+        aux = NULL;
+    }
+    else
+    {
+        // Generar identificador único
+        cout << "\tSU IDENTIFICADOR UNICO ES: \n";
+        identificador(aux->matricula, aux->anio, aux->mes, aux->dia, aux->identificacion);
+        cout << aux->identificacion << endl;
+        cout << "\n";
+
+        aux->izq = NULL;
+        aux->der = NULL;
+        aux->altura = 1;
+    }
+}
 int main()
 {
     raiz = NULL;
     int opc = 0;
-    string identificacion = "";
+    int identificador[10];
+    int identificacion[10];
+
     do
     {
         cout << "\tMENU DE OPCIONES" << endl;
-        cout << "1. REGISTRAR NODO" << endl;
-        cout << "2. BUSCAR UN VIAJE POR IDENTIFICADOR" << endl;
-        cout << "3. LISTAR TODOS LOS VIAJES" << endl;
-        cout << "4. ELIMINAR UN VIAJE POR IDENTIFICADOR" << endl;
-        cout << "5. REGISTRAR UN PASAJERO EN UN VIAJE" << endl;
-        cout << "6. LISTAR TODOS LOS PASAJEROS" << endl;
-        cout << "7. SALIR" << endl;
+        cout << "1. Registrar nodo" << endl;
+        cout << "2. Buscar un viaje por identificador" << endl;
+        cout << "3. Listar todos los viajes" << endl;
+        cout << "4. Eliminar un viaje por identificador" << endl;
+        cout << "5. Registrar un pasajero en un viaje" << endl;
+        cout << "6. Listar todos los pasajeros" << endl;
+        cout << "7. Salir" << endl;
+        cout << "Ingrese una opción: ";
         cin >> opc;
+
         switch (opc)
         {
         case 1:
@@ -221,28 +442,68 @@ int main()
             raiz = insertar(raiz);
             break;
         case 2:
-            buscarNodoPorIdentificacion(raiz, identificacion);
+            cout << "Ingrese el identificador del viaje a buscar: ";
+            for (int i = 0; i < 10; i++)
+            {
+                cin >> identificador[i];
+            }
+
+            if (validarIdentificador(identificador))
+            {
+                nodo *nodoEncontrado = BuscarPorIdentificacion(raiz, identificador);
+                if (nodoEncontrado != NULL)
+                {
+                    imprimirNodo(nodoEncontrado);
+                }
+                else
+                {
+                    cout << "El viaje no fue encontrado." << endl;
+                }
+            }
+            else
+            {
+                cout << "El identificador ingresado no es válido." << endl;
+            }
             break;
         case 3:
-            cout << "INORDEN:\n ";
+            cout << "Viajes registrados (Inorden):\n";
             Inorden(raiz);
             cout << endl;
             break;
         case 4:
-            Inorden(raiz);
+            cout << "Eliminar un viaje:\n";
+            cout << "Ingrese la identificación a eliminar: ";
+            for (int i = 0; i < 10; i++)
+            {
+                cin >> identificacion[i];
+            }
+
+            if (validarIdentificador(identificacion) && BuscarPorIdentificacion(raiz, identificacion))
+            {
+                EliminarPorIdentificacion(raiz, identificacion);
+                cout << "Viaje eliminado satisfactoriamente" << endl;
+            }
+            else
+            {
+                cout << "La identificación ingresada no existe" << endl;
+            }
             break;
         case 5:
+            // Implementar función para registrar un pasajero
             break;
         case 6:
+            // Implementar función para listar todos los pasajeros
             break;
         case 7:
-            cout << " BYE BYE\n ";
+            cout << "Adiós" << endl;
             break;
         default:
-            cout << " OPCION INVALIDA\n";
+            cout << "Opción inválida" << endl;
             break;
         }
+
+        cout << endl;
     } while (opc != 7);
 
     return 0;
-}
+ }
